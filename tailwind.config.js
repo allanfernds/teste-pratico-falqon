88697ee8d3f43d97cadf65bf445338020af8e5b7
Alan Fernandes <allanfernds@gmail.com>
2023-04-19 21:10:17 -0300

    feat: create colour pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     content: ["./src/**/*.{html,js,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'bright-purple': '#7B1FA2',
+                'dark-purple': '#431C60',
+                'light-blue': '#AAB5E0',
+                'royal-blue': '#4527A0',
+                'vivid-green': '#8BC34A',
+                'soft-black': '#272727',
+            },
+        },
     },
     plugins: [],
 }
