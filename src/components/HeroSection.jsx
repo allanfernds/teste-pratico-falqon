d0c5d5081402c5e451e2c1ac3b6eada2c19dfaac
Alan Fernandes <allanfernds@gmail.com>
2023-04-20 00:03:08 -0300

    update App.jsx and tailwind.config.js

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HeroSection() {
+  return (
+    <section id="hero">
+      <div className="bg-hero h-[368px]"></div>
+      <div className="h-[279px] w-[333x] bg-"></div>
+      <div className="h-[137px] bg-bg-purple"></div>
+    </section>
+  );
+}
+
+export default HeroSection;
